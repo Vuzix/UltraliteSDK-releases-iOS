6283bce3350233842594a02200573f283b411e47
Joe Martin <joe_martin@vuzix.com>
2023-07-12 13:55:16 -0400

    Adds dependencies

--- Package.swift
+++ Package.swift
@@ -13,10 +13,11 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "UltraliteSDK",
-            targets: ["UltraliteSDKBinary"])
+            targets: ["UltraliteSDKTarget"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/a2/MessagePack.swift.git",from: Version(stringLiteral: "4.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,5 +27,8 @@ let package = Package(
             url: "https://github.com/Vuzix/UltraliteSDK-releases-iOS/releases/download/v0.1/UltraliteSDK.zip",
             checksum: "040c13d331395bbfed73ce487fbbd7c882807246a59b003d9bbb396a4c2d225b"
         ),
+        .target(name: "UltraliteSDKTarget",
+                dependencies: [.product(name: "MessagePack", package: "MessagePack.swift"),
+                               .targetItem(name: "UltraliteSDKBinary", condition: nil)])
     ]
 )
