21828010c30faafce89e218702b9e8267e76c9d0
Joe Martin <joe_martin@vuzix.com>
2023-07-12 10:30:56 -0400

    Lets get this party started!

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+
+let package = Package(
+    name: "UltraliteSDK",
+    platforms: [
+        .iOS(.v14),
+        .watchOS(.v9),
+        .macOS(.v12),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "UltraliteSDK",
+            targets: ["UltraliteSDKBinary"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "UltraliteSDKBinary",
+            url: "https://github.com/Vuzix/UltraliteSDK-releases-iOS/raw/main/UltraliteSDK.zip",
+            checksum: "040c13d331395bbfed73ce487fbbd7c882807246a59b003d9bbb396a4c2d225b"
+        ),
+    ]
+)
