36a2f286fc90128a3866aee340f9410f4ded3f52
Joe Martin <joe_martin@vuzix.com>
2023-07-14 09:47:59 -0400

    * added missing resources and dependencies

--- Package.swift
+++ Package.swift
@@ -7,29 +7,27 @@ let package = Package(
     platforms: [
         .iOS(.v14),
         .watchOS(.v9),
-        .macOS(.v12),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(name: "UltraliteFonts", targets: ["UltraliteFonts"]),
         .library(
             name: "UltraliteSDK",
-            targets: ["UltraliteSDKTarget"])
+            targets: ["UltraliteSDKBinary"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/a2/MessagePack.swift.git",from: Version(stringLiteral: "4.0.0")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(name: "UltraliteFonts",
+                dependencies: [],
+                resources: [
+                    .copy("Resources"),
+                ]),
+        
         .binaryTarget(
-            name: "UltraliteSDKBinary",
-            url: "https://github.com/Vuzix/UltraliteSDK-releases-iOS/releases/download/v0.1/UltraliteSDK.zip",
-            checksum: "040c13d331395bbfed73ce487fbbd7c882807246a59b003d9bbb396a4c2d225b"
-        ),
-        .target(name: "UltraliteSDKTarget",
-                dependencies: [.product(name: "MessagePack", package: "MessagePack.swift"),
-                               .targetItem(name: "UltraliteSDKBinary", condition: nil)],
-               sources: ["Sources"])
+                name: "UltraliteSDKBinary",
+                url: "https://github.com/Vuzix/UltraliteSDK-releases-iOS/releases/download/v0.1/UltraliteSDK.zip",
+                checksum: "77908f1ec7ee9b3333a7a6ad0125d003c146e0ce4a086f24a7b92fd2b52a4f81"
+            ),
     ]
 )
---
+++ Sources/UltraliteFonts/UltraliteFonts.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Joe Martin on 7/12/23.
+//
+
+public class UltraliteFonts {
+    
+    public init() {
+        
+    }
+    
+}
