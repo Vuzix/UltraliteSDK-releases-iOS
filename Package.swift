3e30fc528bab0c93a9e7b20c622cd2ab2409218d
Joe Martin <joe_martin@vuzix.com>
2024-10-28 16:34:21 -0400

    v1.7.0 SDK
    ScrollLayout
    * Adds: Allow minimum scroll index to be requested, results in the bottom being pushed up from bottom of glasses display
    * Adds: pause/resume/scroll/clear simple functions to ScrollLayout
    
    LiveText
    * Fixes: LiveText parameters not working -- width, height
    * Fixes: smoothness of scroll and responsiveness
    * Adds: new parameter startingLineText -- allows you to push text up from bottom of view on glasses
    * Adds: clear a single slice
    
    Other bugs and enhancements.

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
                 name: "UltraliteSDK",
-                url: "https://github.com/Vuzix/UltraliteSDK-releases-iOS/releases/download/v1.5.1/UltraliteSDK.zip",
-                checksum: "32d874c9a478b004b7d2b0b9b4c71de0f4f3b58a4649f991bf1dadecf3350d97"
+                url: "https://github.com/Vuzix/UltraliteSDK-releases-iOS/releases/download/v1.7.0/UltraliteSDK.zip",
+                checksum: "992b09e0108bec1c499581792aa86d72d1ff4af8ac02992a9ddc90d6967fe58a"
             ),
     ]
 )
